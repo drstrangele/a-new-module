87bf4ed81f57146d81a2d7c065c60f8969ed2f6f
drstrangele <chickenle@gmail.com>
2017-05-20 12:55:06 -0700

    created new module

---
+++ sillyipsum/index.js
@@ -0,0 +1,8 @@
+module.exports.sayHi = "hi";
+module.exports.shakespearianInsult = function() {
+    return "You villainous tickle-brained barnacle!";
+}
+
+module.exports.loremIpsumSentence = function() {
+    return "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod ";
+}
---
+++ test.js
@@ -0,0 +1,7 @@
+var sillyIpsum = require("./sillyipsum");
+var output = sillyIpsum.shakespearianInsult();
+
+var ipsum = sillyIpsum.loremIpsumSentence();
+
+// console.log(output);
+console.log(ipsum);
